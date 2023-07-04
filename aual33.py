af20f09fdac0d4ec9ccc367cc3a2c1eff424bb53
bielgennaro <zirondigabriel@gmail.com>
2023-07-04 03:06:01 -0300

    fuguete num tem ré

---
+++ aual33.py
@@ -0,0 +1,4 @@
+string = 'gabriel zirondi'
+outra_variavel = f'{string[:3]}ABC'
+print(outra_variavel)
+print(string.capitalize())
---
+++ aula34.py
@@ -0,0 +1,11 @@
+condicao = True
+
+while condicao:
+    nome = input("Digite seu nome: ")
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+
+print('Acabou')
